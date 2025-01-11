ac56a8b0086ad15eb5c736e3760ad1c54832a260
fran <fran.bernal@outlook.com>
2025-01-12 00:49:03 +0100

    first steps

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 1. Create the virtual environment, ``python3 -m venv venv1``
 2. view-->command palet --> python select interpreter select the env we have installed
-3. source venv1/bin/activate // deactivaste
+3. source venv1/bin/activate // deactivaste // venv1/Scripts/Activate.ps1
+4. Ejecurar para levantar el server ``fastapi dev main.py``
+5. Instalar FastApi
+6. levantar el server ``fastapi dev main.py``
+
+# POSTMAN
+Used in test section for testing purposes but there are no schema.
+
+# Pydantic
+- for validating , for defining an schema that the user must accomplis
+
---
+++ main.py
@@ -0,0 +1,55 @@
+#test
+
+# from fastapi import FastAPI #import the library
+# from fastapi.params import Body
+
+# app=FastAPI() #create instance of fastapi
+
+# @app.get("/fran")#the route where to find the stuff /fran would be: http://127.0.0.1:8000/fran (decorator , endpoint)
+# def root(): #root=funtion name (does not matter)
+#     return {"message": "Hello World"}
+
+# @app.get("/posts")
+# def get_posts():
+#     return{"data": "this is your posts"}
+
+# @app.post("/createpost")
+# def create_posts(payload: dict =Body(...)): #this gets the body from the post ,convert into a python dictionary and print save it into variable
+#     print (payload)
+#     return{"new_post": f"title: {payload['title']}"+" , succesfully created post"}
+
+
+######
+#using Pydantic
+######
+
+from typing import Optional
+from fastapi import FastAPI #import the library
+from fastapi.params import Body
+from pydantic import BaseModel
+
+app=FastAPI() #create instance of fastapi
+
+class Post (BaseModel): # here we use pydantic for define the schema
+    title: str
+    content: str
+    published: bool = True # this is an optional/odefault to true
+    rating: Optional[int]= None #Optional filed with no default value , type integer
+    
+    
+
+@app.get("/fran")#the route where to find the stuff /fran would be: http://127.0.0.1:8000/fran (decorator , endpoint)
+def root(): #root=funtion name (does not matter)
+    return {"message": "Hello World"}
+
+@app.get("/posts")
+def get_posts():
+    return{"data": "this is your posts"}
+
+@app.post("/createpost")
+def create_posts(new_post: Post): #We are refenrcing the Post class that validates if the post meets the scheme requirements
+    print (new_post)
+    print (new_post.dict()) # We can convert the pydantic model into a dict
+    return{"data": "new post added: " f"title: {new_post.title}"} # accessing the title attribute of  new_poost
+
+#title str, content str, category
