64b229bac39a144c5b3e62ff5cb361adf15a0c95
franky <fran.bernal@outlook.com>
2025-08-29 00:08:02 +0200

    updated configuration

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends,status,HTTPException,Response
 from .. import schemas,database,models,utils
+from . import oauth
 from  sqlalchemy.orm import Session
 router= APIRouter(
     #prefix="/auth",
@@ -15,7 +16,9 @@ def sign_in(user_cred:schemas.UserSignin,db: Session = Depends(database.get_db))
         
     if not utils.check(user_cred.password,user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= "Invalid credentials (password)") # checks the passwords match
-
-    #Create token
+    
+    
+    #Create token, we have used the user id as content of the token but can be any ohter field
+    token=oauth.create_access_token(data={"user_id": user.id}) # we are passing the id in form of dictionary that is what is expecting
     #Return token
-    return user
+    return token
